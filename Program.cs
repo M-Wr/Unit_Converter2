c3cf63bdc0650d03ffc22fd290267084193a6b43
Max Wright <max.wright@experian.com>
2022-11-24 13:59:10 +0000

    First Commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace game
+{
+    namespace Review
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("How many kilograms do you want to convert?: ");
+                var input = Console.ReadLine();
+                var kilo = Int32.Parse(input);
+                var stone = kilo / 6.35;
+                var stoneDec = stone % 1;
+
+                var pound = stoneDec * 14;
+                stone = Math.Truncate(stone);
+                pound = Math.Round(pound, 2);
+
+                var endp = $" {kilo} Kg, is equivilent to: {stone}st and {pound}lbs";
+                Console.WriteLine(endp);
+
+            }
+        }
+    }
+}
